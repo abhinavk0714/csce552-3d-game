50879aa5c5a15ca1dfa00407462ed4ba1ef270a3
abhinavk0714 <abhinavk@email.sc.edu>
2025-11-17 13:46:34 -0500

    imported and applied all sfx
    added all sfx and applied them to their respective areas, added a little pop-up in the main menu to adjust audio sources

--- scripts/components/SpringBehavior.gd
+++ scripts/components/SpringBehavior.gd
@@ -12,3 +12,10 @@ func _ready():
 func _on_body_entered(body):
 	if body is RigidBody3D:
 		body.apply_central_impulse(Vector3.UP * launch_force)
+		
+		# Play spring sound at reduced volume
+		var audio_manager = get_node("/root/AudioManager")
+		if audio_manager:
+			var spring_sound = load("res://assets/audio/sfx/spring.wav")
+			if spring_sound:
+				audio_manager.play_sfx(spring_sound, global_position, 0.3)  # 30% volume
--- scripts/ui/main_menu.gd
+++ scripts/ui/main_menu.gd
@@ -3,11 +3,14 @@ extends Control
 # MainMenu script
 # Handles main menu UI interactions and scene transitions
 
+var settings_popup: Panel
+
 func _ready():
 	var play_button = get_node_or_null("CanvasLayer/VBoxContainer/PlayButton")
 	var level_select_button = get_node_or_null("CanvasLayer/VBoxContainer/LevelSelectButton")
 	var options_button = get_node_or_null("CanvasLayer/VBoxContainer/OptionsButton")
 	var quit_button = get_node_or_null("CanvasLayer/VBoxContainer/QuitButton")
+	settings_popup = get_node_or_null("CanvasLayer/SettingsPopup")
 	
 	if play_button:
 		play_button.pressed.connect(_on_play_button_pressed)
@@ -23,17 +26,27 @@ func _ready():
 		var audio_manager = get_node("/root/AudioManager")
 		audio_manager.play_music(main_menu_music)
 
+func _play_ui_click():
+	var audio_manager = get_node("/root/AudioManager")
+	if audio_manager:
+		audio_manager.play_ui_click()
+
 func _on_play_button_pressed():
+	_play_ui_click()
 	get_tree().change_scene_to_file("res://scenes/levels/test_world.tscn")
 
 func _on_level_select_button_pressed():
+	_play_ui_click()
 	# TODO: Open level select menu
 	print("Level select button pressed")
 
 func _on_options_button_pressed():
-	# TODO: Open options menu
-	print("Options button pressed")
+	_play_ui_click()
+	# Show settings popup
+	if settings_popup:
+		settings_popup.show_popup()
 
 func _on_quit_button_pressed():
+	_play_ui_click()
 	get_tree().quit()
 
