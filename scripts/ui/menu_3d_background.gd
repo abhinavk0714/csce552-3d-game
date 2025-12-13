925cef5d69fc2222870bfb8ca72ae91a35773fe0
abhinavk0714 <abhinavk@email.sc.edu>
2025-12-12 23:09:20 -0500

    added Aro animation in main menu

--- scripts/ui/menu_3d_background.gd
+++ scripts/ui/menu_3d_background.gd
@@ -1,17 +1,40 @@
 extends Node3D
 
 # Script to animate the Aro model in the main menu background
-# Makes Aro rotate slowly for a nice visual effect
+# Plays victory dance animation
 
-@export var rotation_speed: float = 0.3  # Degrees per second
-var aro_model: Node3D
+var animation_player: AnimationPlayer
 
 func _ready():
-	# Find the Aro model (it might be nested in the GLB scene)
-	aro_model = get_node_or_null("AroModel")
+	# Use call_deferred to ensure nodes are fully loaded
+	call_deferred("_setup_animation")
+
+func _setup_animation():
+	# Find the AroModel node
+	var aro_model = get_node_or_null("AroRotator/AroModel")
 	if not aro_model:
-		# Try to find it recursively
+		aro_model = get_node_or_null("AroModel")
+	if not aro_model:
 		aro_model = _find_aro_recursive(self)
+	
+	if not aro_model:
+		push_warning("Menu3D: Aro model not found")
+		return
+	
+	# Find AnimationPlayer in the Aro model
+	animation_player = _find_animation_player(aro_model)
+	if animation_player:
+		# Set Victory animation to ping-pong (plays forward then backward)
+		if "Victory" in animation_player.get_animation_list():
+			var victory_anim = animation_player.get_animation("Victory")
+			if victory_anim:
+				victory_anim.loop_mode = Animation.LOOP_PINGPONG
+			# Play the victory animation
+			animation_player.play("Victory")
+		else:
+			push_warning("Menu3D: Victory animation not found. Available: %s" % animation_player.get_animation_list())
+	else:
+		push_warning("Menu3D: AnimationPlayer not found in Aro model")
 
 func _find_aro_recursive(node: Node) -> Node3D:
 	if node.name.contains("Aro") or node.name.contains("frog"):
@@ -22,8 +45,12 @@ func _find_aro_recursive(node: Node) -> Node3D:
 			return result
 	return null
 
-func _process(delta):
-	if aro_model:
-		# Rotate around Y axis (vertical)
-		aro_model.rotate_y(deg_to_rad(rotation_speed * delta))
+func _find_animation_player(node: Node) -> AnimationPlayer:
+	if node is AnimationPlayer:
+		return node as AnimationPlayer
+	for child in node.get_children():
+		var result = _find_animation_player(child)
+		if result:
+			return result
+	return null
 
